7f10bc51d6db61c6bb1280c7a4933c2e15352a88
9beat <126194877+9beat@users.noreply.github.com>
2023-07-24 09:29:48 +0200

    views/main-layouts

---
+++ src/views/Layout/AnimatedRoutes.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { useSelector } from 'react-redux';
+
+import { Route, Routes, useLocation, Navigate } from 'react-router-dom'
+// Views
+import Login from "../Auth/Login.jsx";
+import Home from "../Home/Home.jsx";
+import Profile from "../Profile/Profile.jsx";
+import ErrorPage from '../ErrorPage/ErrorPage.jsx';
+// Framer
+import { AnimatePresence } from "framer-motion";
+
+const AnimatedRoutes = () => {
+    const location = useLocation()
+
+    // auth state
+    const isAuth = Boolean(useSelector((state) => state.token));
+
+    return (
+        <AnimatePresence>
+            <Routes location={location} key={location.pathname}>
+                <Route path="/" element={ <Login /> } />
+                <Route path="/home" element={ isAuth ? <Home /> : <Navigate to="/" /> } />
+                <Route path="/profile/:userId" element={ isAuth ? <Profile /> : <Navigate to="/" /> } />
+                <Route path="*" element={ <ErrorPage  /> } />
+            </Routes>
+        </AnimatePresence>
+    )
+}
+
+export default AnimatedRoutes
